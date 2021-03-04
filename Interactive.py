9b6605b46f6da6377bf8c2640c23ab87ab51928f
lakvinu <sandeepalhah@gmail.com>
2021-03-04 16:47:24 +1100

    added new bg

---
+++ Interactive.py
@@ -0,0 +1,175 @@
+import tkinter
+import time
+
+window = tkinter.Tk()
+window.title("Sudoku Solver by Lakvinu")
+#window.geometry("1050x880")
+
+def rgbtohex(r,g,b):
+    return f'#{r:02x}{g:02x}{b:02x}'
+
+window.configure(background= rgbtohex(40,94,145))
+
+window.resizable(0,0)
+
+board_two = [[0 for i in range(9)] for j in range(9)]
+
+board = [[0 for i in range(9)] for j in range(9)]
+
+with_boost = True
+
+
+
+def set_text(row, col, number):
+    board_two[row][col].delete(0, "end")
+    board_two[row][col].insert(0, str(number))
+    if with_boost:
+        window.update()
+        time.sleep(0.0001)
+
+
+def empty_pos():
+
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+
+    return None, None
+
+
+def sudoku_solver():
+    global board_two
+
+
+    row, col = empty_pos()
+
+    if row == None or col == None:
+        return True
+
+    for i in range(1, 10):
+
+        set_text(row, col, i)
+
+        if is_valid(row, col, i) == True:
+
+            board[row][col] = i
+
+            #if current number works
+
+            if sudoku_solver() == True:
+                return True
+
+            #if current number fails
+
+            else:
+
+                board[row][col] = 0
+
+    return False
+
+
+
+def is_valid(row, col, num):
+
+    #checking within rows
+
+    for i in range(9):
+        if i != col:
+            if board[row][i] == num:
+                return False
+
+    #checking within cols
+
+    for i in range(9):
+        if i != row:
+            if board[i][col] == num:
+                return False
+
+    #need to check within the current box
+
+    #finding the start of the box
+    box_row = (row // 3) * 3
+    box_col = (col // 3) * 3
+
+    for i in range(box_row, box_row + 3):
+        for j in range(box_col, box_col + 3):
+            if i != row and j != col:
+                if board[i][j] == num:
+                    return False
+
+    #if no mistakes return True
+
+    return True
+
+
+def begin():
+
+    global board_two
+    global board
+
+    for i in range(9):
+        for j in range(9):
+            cur = board_two[i][j].get()
+
+            if cur != '':
+                board[i][j] = int(cur)
+
+    sudoku_solver()
+
+
+def erase():
+
+    global board_two
+    global with_boost
+
+    for i in range(9):
+        for j in range(9):
+            board_two[i][j].delete(0, "end")
+            board_two[i][j].insert(0, '')
+            board[i][j] = 0
+
+    with_boost = True
+
+
+def instant():
+    global with_boost
+    with_boost = False
+
+#build the layout
+
+def create_grid():
+
+    for i in range(9):
+        for j in range(9):
+            left, right = 1,1
+            top, bottom = 1, 1
+            border_check = {2,5,8}
+
+            if i in border_check:
+                bottom = 5
+
+            if j in border_check:
+                right = 5
+
+
+            box = tkinter.Entry(window, width=2, font=('Arial', 60), bd=3, highlightbackground = rgbtohex(170,38,54), highlightthickness=4)
+            box.grid(row=i, column=j, padx = (left, right), pady =(top, bottom))
+            board_two[i][j] = box
+
+    start = tkinter.Button(window, height=1, width=7, text="Start", font=("Arial", 30), command=begin)
+
+    fast = tkinter.Button(window, height=1, width=7, text="Fast", font=("Arial", 30), command=instant)
+
+    clear = tkinter.Button(window, height=1, width=7, text="Clear", font=("Arial", 30), command=erase)
+
+    start.grid(row=3, column=9, padx = 10)
+
+    clear.grid(row=5, column=9)
+
+    fast.grid(row=4, column=9)
+
+
+create_grid()
+
+window.mainloop()
